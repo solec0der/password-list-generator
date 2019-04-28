b8a373307f21831f3c035785c2b586d621538c08
Mortimer Cold <mortimer.cold@gmail.com>
2019-04-28 16:38:24 +0200

    added a first version of password generation. (combining every word with each other in every combination of upper and lower case / adding numbers)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-# Code will be moved into classes, if basic functionallity is implemented.
+# Code will be moved into classes, when basic functionallity is implemented.
 
 class PasswordParameter:
     def __init__(self, display_value, value, allow_multiple):
         self.display_value = display_value
         self.value = value
         self.allow_multiple = allow_multiple
+
+def combine_parameters(parameter_a, parameter_b):
+    combinations = []
+    combinations.append(parameter_a.lower() + parameter_b.lower())
+    combinations.append(parameter_a.lower() + parameter_b.upper())
+    combinations.append(parameter_a.upper() + parameter_b.lower())
+    combinations.append(parameter_a.upper() + parameter_b.upper())
+    return combinations
 
 # Create a dictionary to store all user input
 user_inputs = {}
@@ -13,8 +21,11 @@ user_inputs = {}
 password_parameters = [
     PasswordParameter('first name', 'first_name', False),
     PasswordParameter('last name', 'last_name', False),
+    PasswordParameter('nick name', 'nick_name', False),
     PasswordParameter('pet name', 'pet_name', False),
-    PasswordParameter('date of birth (format: YYYY/MM/DD)', 'date_of_birth', False),
+    PasswordParameter('year of birth', 'year_of_birth', False),
+    PasswordParameter('month of birth', 'month_of_birth', False),
+    PasswordParameter('day of birth', 'day_of_birth', False),
     PasswordParameter('place of birth', 'place_of_birth', False),
     PasswordParameter('miscellaneous or random words associated with the victim', 'random_words', True)
 ]
@@ -25,7 +36,27 @@ for parameter in password_parameters:
     if user_input.strip():
         user_inputs[parameter.value] = user_input
 
+# Combine every word with each word (dumb test)
 
+combinations = []
 
+for user_input_a in user_inputs:
+    for user_input_b in user_inputs:
+        parameter_a = user_inputs[user_input_a]
+        parameter_b = user_inputs[user_input_b]
 
+        if not parameter_a == parameter_b:
+            combinations.extend(combine_parameters(parameter_a, parameter_b))
 
+            # Try to add numbers from 0 - 2500 / Maybe add some more logic later
+            for i in range(0, 2500):
+                combinations.extend(combine_parameters(str(i) + parameter_a, parameter_b))
+                combinations.extend(combine_parameters(parameter_a, parameter_b + str(i)))
+
+# Output amount of passwords generated
+print(len(combinations))
+
+# Figure out how to write to file
+with open('password_list.txt', 'w') as output_file:
+    for combination in combinations:
+        output_file.write(combination + "\n")
